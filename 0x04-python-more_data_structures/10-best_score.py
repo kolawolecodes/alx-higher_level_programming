1523ad08a0419afccab41405a200e0395f9f1103
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-09 00:11:58 +0000

    function that returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    returns a key with the biggest integer value
+    Args:
+        a_dictionary: intial dictionary
+    Return:
+        key with biggest integer value or none
+        if score is not found
+    """
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=lambda x: a_dictionary[x])
