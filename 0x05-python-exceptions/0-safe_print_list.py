06cf94008428ec47106b0cc519583f23327fec9b
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-27 17:48:08 +0000

    print the Ntn number of elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """
+    Prints elements of a list
+    Args:
+        my_list: intial list
+        x:  Number of elements to print
+    Return:
+        number of elements to print
+    """
+    iter = 0
+    while iter < x:
+        try:
+            print(my_list[iter], end='')
+            iter += 1
+        except IndexError:
+            break
+    print()
+    return iter
