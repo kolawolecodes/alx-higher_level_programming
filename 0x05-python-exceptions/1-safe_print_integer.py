7226f7bcd80bd3b3d4ed6cb90a70c27c3fc44898
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-27 17:52:38 +0000

    function that print integer with errors handled

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Prints an integer with string format for integer
+    Args:
+        value: any data type
+    Return:
+        True if integer else false
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
