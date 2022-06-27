59016c665bc2da41b162698f87f5513bcf4bba3b
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-27 23:21:52 +0000

    function that divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Prints x number of elements of a list and only integers
+    Args:
+        my_list: initial list
+        x:  number of elements to access in list
+    Return:
+        number of integers printed
+    """
+    res = 0
+    for iter in range(x):
+        try:
+            print("{:d}".format(my_list[iter]), end='')
+            res += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return res
